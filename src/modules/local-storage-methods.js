2c560d83afb632527fc8a7ab57cbeec0229ecbb4
6fex <54210730+6fex@users.noreply.github.com>
2026-02-12 16:28:26 +0100

    Refactor localStorage methods module

--- src/modules/local-storage-methods.js
+++ src/modules/local-storage-methods.js
@@ -1,9 +1,10 @@
-export default function(){
-    function push(key, item) {
+export default {
+    push(key, item) {
         const array = JSON.parse(localStorage.getItem(key));
         array.push(item);
         localStorage.setItem(key, JSON.stringify(array));
-    };
-
-    return { push }
+    },
+    createArray(key) {
+        localStorage.setItem(key, JSON.stringify([]));
+    }
 };
