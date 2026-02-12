104cc565f4dc1e2a8ee41f44b8a1d3e521a7956d
6fex <54210730+6fex@users.noreply.github.com>
2026-02-12 15:51:56 +0100

    Refactor import

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./styles.css";
-import List from "./modules/list.js";
+import { List } from "./modules/list.js";
 import ToDoItem from "./modules/todo-item.js";
+
+const listsDiv = document.querySelector(".lists");
+
+const testList = new List("Today");
+console.log(testList);
+
+const testItem = new ToDoItem("mow lawn", "before 5 o'clock", "12-02-2026", "medium", false);
+console.log(testItem);
+
+testList.addToDo(testItem);
+console.log(testList.getToDos());
+
+testList.removeTodo("mow lawn");
+console.log(testList.getToDos());
