49c4b24d6d6a8072f4877cb30d99063bd778e606
6fex <54210730+6fex@users.noreply.github.com>
2026-02-12 15:55:58 +0100

    Add create html element function to list module

--- src/modules/list.js
+++ src/modules/list.js
@@ -22,4 +22,11 @@ class List {
     };
 };
 
-export { List };
+function createListDiv(name) {
+    const listDiv = document.createElement("div");
+    listDiv.textContent = name;
+
+    return listDiv;
+};
+
+export { List, createListDiv };
